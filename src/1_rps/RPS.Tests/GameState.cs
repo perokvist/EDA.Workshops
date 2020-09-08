2c5edb366b58d9789d5198dad8a6b3106658abf7
Magnus Altgaard <magnus.altgard@tretton37.com>
2020-09-08 07:24:29 +0200

    excercise 1-1

--- src/1_rps/RPS.Tests/GameState.cs
+++ src/1_rps/RPS.Tests/GameState.cs
@@ -3,6 +3,11 @@
     public class GameState
     {
         public GameState When(IEvent @event) => this;
+
+        public GameState When(GameCreated @event) => new GameState { Status = GameStatus.ReadyToStart };
+        public GameState When(GameStarted @event) => new GameState { Status = GameStatus.Started };
+        public GameState When(GameEnded @event) => new GameState { Status = GameStatus.Ended };
+
         public GameStatus Status { get; set; }
     }
 
