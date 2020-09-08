fb6c5c1fee3a74f55a91aef6e8dcc013a90e6b8e
Magnus Altgaard <magnus.altgard@tretton37.com>
2020-09-08 13:58:00 +0200

    exercise 2

--- src/2_rps/RPS.Tests/Game.cs
+++ src/2_rps/RPS.Tests/Game.cs
@@ -7,12 +7,82 @@ namespace RPS.Tests
     public static class Game
     {
         public static IEnumerable<IEvent> Handle(CreateGame command, GameState state)
-            => Array.Empty<IEvent>();
+            => new List<IEvent>
+            {
+                new GameCreated
+                {
+                    Created = DateTime.UtcNow,
+                    GameId = command.GameId,
+                    PlayerId = command.PlayerId,
+                    Rounds = command.Rounds,
+                    Title = command.Title
+                }
+            };
+
 
         public static IEnumerable<IEvent> Handle(JoinGame command, GameState state)
-            => Array.Empty<IEvent>();
+        {
+            if (command.PlayerId == state.Player1)
+            {
+                return Array.Empty<IEvent>();
+            }
+            var gameStarted = new GameStarted
+            {
+                GameId = command.GameId,
+                PlayerId = command.PlayerId,
+            };
+            var roundStarted = new RoundStarted
+            {
+                GameId = command.GameId,
+                Round = 1
+            };
+            return new List<IEvent> { gameStarted, roundStarted };
+        }
 
         public static IEnumerable<IEvent> Handle(PlayGame command, GameState state)
-            => Array.Empty<IEvent>();
+        {
+            var handShown = new HandShown
+            {
+                GameId = command.GameId,
+                Hand = command.Hand,
+                PlayerId = command.PlayerId
+            };
+            if (state.Player1Hand == Hand.None && state.Player2Hand == Hand.None)
+            {
+                return new List<IEvent> { handShown };
+            }
+
+            Hand hand1 = Hand.None;
+            Hand hand2 = Hand.None;
+
+            if (command.PlayerId == state.Player1)
+            {
+                hand1 = command.Hand;
+                hand2 = state.Player2Hand;
+            }
+
+            if (command.PlayerId == state.Player2)
+            {
+                hand1 = state.Player1Hand;
+                hand2 = command.Hand;
+            }
+
+
+            var roundEnded = new RoundEnded
+            {
+                GameId = command.GameId,
+                Winner = (hand1, hand2) switch
+                {
+                    (Hand.Paper, Hand.Rock) => state.Player1,
+                    (Hand.Rock, Hand.Paper) => state.Player2,
+                    (_, _) => throw new InvalidOperationException()
+                }
+            };
+
+            var gamedEnded = new GameEnded { GameId = command.GameId };
+
+            return new List<IEvent> { handShown, roundEnded, gamedEnded };
+
+        }
     }
 }
--- src/2_rps/RPS.Tests/GameState.cs
+++ src/2_rps/RPS.Tests/GameState.cs
@@ -1,8 +1,49 @@
-﻿namespace RPS.Tests
+﻿using System;
+
+namespace RPS.Tests
 {
     public class GameState
     {
         public GameState When(IEvent @event) => this;
+        public GameState When(GameCreated @event)
+        {
+            Rounds = @event.Rounds;
+            Player1 = @event.PlayerId;
+            GameId = @event.GameId;
+
+            return this;
+        }
+
+        public GameState When(GameStarted @event)
+        {
+            Player2 = @event.PlayerId;
+            return this;
+        }
+        public GameState When(RoundStarted @event)
+        {
+            return this;
+        }
+
+        public GameState When(HandShown @event)
+        {
+            if (@event.PlayerId == Player1)
+            {
+                Player1Hand = @event.Hand;
+            }
+            
+            if (@event.PlayerId == Player2)
+            {
+                Player2Hand = @event.Hand;
+            }
+            return this;
+        }
+
+        public Guid GameId { get; private set; }
+        public int Rounds { get; private set; }
+        public string Player1 { get; private set; }
+        public string Player2 { get; private set; }
+        public Hand Player1Hand { get; private set; }
+        public Hand Player2Hand { get; private set; }
 
         public enum GameStatus
         {
