b90de6f5fcd7e75df0e2846dce40d3f231c80d8d
Magnus Altgaard <magnus.altgard@tretton37.com>
2020-09-09 09:57:59 +0200

    exercise 5

--- src/5_invariants/Invariants.Tests/Game.cs
+++ src/5_invariants/Invariants.Tests/Game.cs
@@ -8,12 +8,20 @@ namespace Invariants.Tests
     {
         public static IEnumerable<IEvent> Handle(JoinGame command, GameState state)
         {
+            if (state.Player1 == command.PlayerId)
+                yield break;
+            
             yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
             yield return new RoundStarted { GameId = command.GameId, Round = 1 };
         }
 
         public static IEnumerable<IEvent> Handle(JoinGame command, IEvent[] events)
         {
+            var state = events.Rehydrate<GameState>();
+
+            if(state.Player1 == command.PlayerId)
+                yield break;
+            
             yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
             yield return new RoundStarted { GameId = command.GameId, Round = 1 };
         }
--- src/5_invariants/Invariants.Tests/GameState.cs
+++ src/5_invariants/Invariants.Tests/GameState.cs
@@ -1,8 +1,21 @@
-﻿namespace Invariants.Tests
+﻿using System;
+
+namespace Invariants.Tests
 {
     public class GameState
     {
         public GameState When(IEvent @event) => this;
+
+        public GameState When(GameCreated @event)
+        {
+            GameId = @event.GameId;
+            Player1 = @event.PlayerId;
+
+            return this;
+        }
+
+        public Guid GameId { get; private set; }
+        public string Player1 { get; set; }
     }
 
 }
